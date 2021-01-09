49911f34c8ec0e8a1cf50fc392a4ce2a4ccb58cf
Arpita <arpita@Abhis-MacBook-Pro.local>
2021-01-08 19:39:04 -0800

    Added remove function

--- LinkedList/LinkedListClass.py
+++ LinkedList/LinkedListClass.py
@@ -17,6 +17,24 @@ class LinkedList():
             self.tail.next=node
             self.tail=node
 
+    def remove(self, val):
+        curr=self.head
+        prev=None
+        while curr!=None:
+            if curr.val==val:
+                break
+            prev=curr
+            curr=curr.next
+        if curr!=None:
+            if curr==self.head:
+                self.head=self.head.next
+            else:
+                prev.next=curr.next
+            if curr==self.tail:
+                self.tail=prev
+   
+            del curr
+
     def __str__(self):
         curr=self.head
         ll=[]
@@ -24,8 +42,14 @@ class LinkedList():
             ll.append(str(curr.val))
             curr=curr.next
         return "->".join(ll)
-ls=[5,10,19,20]
+
+ls=[5, 10, 19, 20, 6, 90, 1, 8]
 l=LinkedList()
 for i in ls:
     l.add(i)
+l.remove(5)
+l.remove(8)
+l.remove(20)
+print(l)
+l.add(7)
 print(l)
